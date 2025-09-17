b49c3bfd634b205dbe2dbb282fa618478862c778
rriazn <rnriazanov@gmail.com>
2025-09-17 15:25:54 +0200

    integrating database

--- services/databaseService.js
+++ services/databaseService.js
@@ -26,3 +26,26 @@ async function connectDB() {
   const [rows] = await db.query('SELECT * FROM competitions');
   console.log('Query result:', rows);
 })();
+
+
+exports.getCompetitions = async () => {
+    let [comps] = await db.query('SELECT * FROM competitions');
+    comps = JSON.stringify(comps);
+    comps.array.forEach(async comp => {
+        // get all groups
+        let [compGroups] = await db.query(`SELECT Name AS title FROM competitionGroups WHERE competiton = ${comp.ID}`);
+        compGroups = JSON.stringify(compGroups);
+
+        // get participants for each group
+        compGroups.forEach(async group => {
+            let [participants] = await db.query(`SELECT firstName, lastName, birthDate, Club AS affilitation
+                                                FROM Participants JOIN Participation ON Participants.ID = Participation.Participant
+                                                JOIN CompetitionGroup ON Participation.CompetitionGroup = CompetitionGroup.ID
+                                                WHERE CompetitionGroup.Name = ${group.name} AND Participation.Competition = ${comp.ID}`);
+            participants = JSON.stringify(participants);                                  
+            participants.forEach(async parts => {
+                let [scores] = await db.query(``)
+            });
+        });
+    });
+}
